b0d68a3399bb7b8c5066c734e38e77ae35adb3f3
zpq <530579538@qq.com>
2018-07-11 17:21:53 +0800

    第6次提交

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,6 +2,10 @@ public class User {
     private String name;
     private String age;
     private String ssex;
+    private String zpq;
+
+
+
     public void setName(String name) {
 
         this.name = name;
