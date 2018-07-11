f67ced41cb082b98cc2471b1e6823ddbd47d4dc6
zpq <530579538@qq.com>
2018-07-11 16:48:56 +0800

    第3次提交

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,7 +1,12 @@
 public class User {
     private String name;
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public User(String name) {
         this.name = name;
+
     }
 }
